73d6f2974b588210f5b56ed4bfb9c606ba5c7a6d
YellowPipe <philippdmitrienko@gmail.com>
2019-08-15 14:20:22 +0300

    removed api code

--- src/redux/reducers/root-reducer.js
+++ src/redux/reducers/root-reducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import library from './library';
 import filter from './filter';
-import session from './session';
 
 export default combineReducers({
 	library,
-	filter,
-	session
+	filter
 })
